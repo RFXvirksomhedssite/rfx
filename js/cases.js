4445ce08b56d326a9d96a9d091510160e3be6efe
soumia <soel0003@stud.ek.dk>
2025-12-15 09:40:59 +0100

    indhold

---
+++ js/cases.js
@@ -0,0 +1,33 @@
+const swipefor = document.getElementById("swipefor");
+const swipeforbilleder = document.querySelectorAll("#swipeforbilleder");
+
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+
+swipeforbilleder.forEach((billede) => {
+  billede.addEventListener("click", () => {
+    lightboxImg.src = billede.src;
+    lightbox.style.display = "flex";
+  });
+});
+
+lightbox.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
+
+/**Leas mere funktion**/
+
+const laesMereKnap = document.querySelector(".caseknap");
+const mereTekst = document.querySelector(".mere-tekst");
+
+laesMereKnap.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (mereTekst.style.display === "block") {
+    mereTekst.style.display = "none";
+    laesMereKnap.textContent = "Læs mere";
+  } else {
+    mereTekst.style.display = "block";
+    laesMereKnap.textContent = "Læs mindre";
+  }
+});
